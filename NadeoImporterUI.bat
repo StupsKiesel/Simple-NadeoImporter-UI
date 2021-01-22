76ed076196933d72ebbc68720c9ea9dcec06d00b
StupsKiesel <43930570+StupsKiesel@users.noreply.github.com>
2021-01-22 19:19:00 +0100

    deleted BAT to EXE stuff
    deleted stuff that was created by BAT to EXE

--- NadeoImporterUI.bat
+++ NadeoImporterUI.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jHweuDp0I0WfIwapveyIGnHNQ+1WzpZZk/wn9IreJCBRhXHg==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZkkaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlbMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIeKR9RQy2PLGSoErAO3cGb
-::eg0/rx1wNQPfEVWB+kM9LVsJDCqDOG61L7gM5/rv6viyiy0=
-::fBEirQZwNQPfEVWB+kM9LVsJDCqDOG61L7gM5/rv6viyiy0=
-::cRolqwZ3JBvQF1fEqQIeKR9RQy2PLGSoErAOqN3o6vjHi0MJUfA5eYzeug==
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jHweuDp0I0WfIwapveyIGnHM4S7kDrXJs92WhOndk5JVVdZhfL
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @ECHO OFF
 :START
 title NadeoImporter
